b3ab95e36eaa878a198a3e0504ff84735f18b313
Geoffrey Noel <gnoel5@bloomberg.net>
2020-04-16 07:59:36 -0400

    Fix subPoint

--- src/Point.hs
+++ src/Point.hs
@@ -14,8 +14,8 @@ addVector :: Point -> Vector -> Point
 addVector Point{..} Vector{..} = Point (px+vx) (py+vy) (pz+vz)
 
 -- |Subtract a @Point@ to a @Point@ by substracting the component pairwise
-subPoint :: Point -> Point -> Point
-subPoint (Point x1 y1 z1) (Point x2 y2 z2) = Point (x1-x2) (y1-y2) (z1-z2)
+subPoint :: Point -> Point -> Vector
+subPoint (Point x1 y1 z1) (Point x2 y2 z2) = Vector (x1-x2) (y1-y2) (z1-z2)
 
 -- |Substract a @Vector@ from a @Point@ by substraction the components pairwise
 subVector :: Point -> Vector -> Point
--- test/PointSpec.hs
+++ test/PointSpec.hs
@@ -4,6 +4,7 @@ import Test.Hspec
 import Test.QuickCheck
 
 import Point
+import Vector (negVector)
 import Types
 import Util
 
@@ -14,17 +15,14 @@ spec :: Spec
 spec = do
     describe "Point Operations" $ do
         it "Add a Vector to a Point" $ property $
-            \p1 p2 p3 v1 v2 v3 ->
-                let result   = (Point p1 p2 p3) `addVector` (Vector v1 v2 v3)
-                    expected = Point (p1+v1) (p2+v2) (p3+v3)
-                in expected ~== result
+            \p@(Point p1 p2 p3) v@(Vector v1 v2 v3) ->
+                Point (p1+v1) (p2+v2) (p3+v3) ~== p `addVector` v
         it "Substracting a Point to a Point" $ property $
-            \x1 y1 z1 x2 y2 z2 ->
-                let result   = (Point x1 y1 z1) `subPoint` (Point x2 y2 z2)
-                    expected = Point (x1-x2) (y1-y2) (z1-z2)
-                in expected ~== result
+            \p1@(Point x1 y1 z1) p2@(Point x2 y2 z2) ->
+                Vector (x1-x2) (y1-y2) (z1-z2) ~== p1 `subPoint` p2
         it "Substracting a Vector to a Point" $ property $
-            \p1 p2 p3 v1 v2 v3 ->
-                let result   = (Point p1 p2 p3) `subVector` (Vector v1 v2 v3)
-                    expected = Point (p1-v1) (p2-v2) (p3-v3)
-                in expected ~== result
+            \p@(Point p1 p2 p3) v@(Vector v1 v2 v3) ->
+                Point (p1-v1) (p2-v2) (p3-v3) ~== p `subVector` v
+    describe "Point Properties" $ do
+        it "Point1 - Point2 = negate (Point 2 - Point1)" $ property $
+            \p1 p2 -> p1 `subPoint` p2 ~== negVector (p2 `subPoint` p1)
